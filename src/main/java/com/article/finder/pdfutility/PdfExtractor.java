87a7f54473762353bf09360d44997c2a4072513a
MalaySaxena <44743128+MalaySaxena@users.noreply.github.com>
2020-11-24 19:46:48 +0530

    Update PdfExtractor.java

--- src/main/java/com/article/finder/pdfutility/PdfExtractor.java
+++ src/main/java/com/article/finder/pdfutility/PdfExtractor.java
@@ -4,5 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PdfExtractor {
+<<<<<<< Updated upstream
+=======
+    private String trya = "dsdsdsd";
+    private int a =10;
+>>>>>>> Stashed changes
 
 }
