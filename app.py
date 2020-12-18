b3e2711620a883276f184f27af07012e8f90245e
AjinkyaTaranekar <ajinkyataranekar@gmail.com>
2020-12-19 01:35:26 +0530

    APIs working accuracy failed

--- app.py
+++ app.py
@@ -29,46 +29,35 @@ app = FastAPI(
 )
 
 # Services
-from services.extract_keywords import extract_keywords
-from services.fuzzy import get_fuzzy_similarity
-from services.files import getPdf, getDictionary
-
-# downloading the large model 
-#subprocess.call("python -m spacy download en_core_web_lg",shell=True)
-nlp = spacy.load("en_core_web_lg")
-print("Loaded language model")
+from test import get_results, store_vector
 
 # getting model
 from models.user import User
-
-
-# Initialize Firestore DB
-from firebase_admin import credentials, firestore, initialize_app
-
-cred = credentials.Certificate('secret_key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
-pdfs_ref = db.collection('pdfs')
-user_pdf_ref = db.collection('usersPDF')
+from models.search import Search
 
 @app.post('/api/keywords', tags=['Extraction'])
 def get_keywords(user: User):
-
-    query_string = getPdf(user.user_id, user.file_id)
-    keywords = extract_keywords(nlp,query_string)
+    status = store_vector(user.user_id, user.file_id)
+    if status:
+        return jsonable_encoder({
+            "status": True
+            })
     return jsonable_encoder({
-        "status": True
+        "status": False
         })
 
+
 @app.post('/api/search', tags=['Search'])
-def get_fuzzy_matches(user: User):
-    search = user.search
-    dictionary = getDictionary(user.user_id)
-    pdfs = get_fuzzy_similarity(search,dictionary)
+def get_fuzzy_matches(user: Search):
+    pdfs = get_results(user.user_id,user.search)
+    if pdfs:
+        return jsonable_encoder({
+            'pdfs' : pdfs,
+            'status': True
+        })
     return jsonable_encoder({
-        'pdfs' : pdfs,
-        'status': True
-    })
+            'status': False
+        })
 
 @app.get("/status", tags = ['Welcome'])
 def home():
@@ -76,9 +65,6 @@ def home():
         "status": "True",
         "message": "OK!"
         }
-
-
-
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host="0.0.0.0", port=5002)
--- test.py
+++ test.py
@@ -8,6 +8,8 @@ from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 
+import urllib.request
+
 cred = credentials.Certificate('secret_key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
@@ -22,48 +24,57 @@ def get_results(uid,query):
     query_vector = vectorizer.transform([query])
     files = user_pdf_ref.document(uid).get().to_dict()['pid']
     file_vectors = []
+    properFiles = []
     for file in files:
         file_details= pdfs_ref.document(file).get().to_dict()
-        file_vec_dict = file_details['vector']
+        try:
+            file_vec_dict = file_details['vector']
+        except:
+            continue
         vec_dim = 10000
 
         file_vec = [0]*vec_dim
         for k,v in file_vec_dict.items():
-            file_vec[k] = v
+            file_vec[int(k)] = float(v)
         file_vectors.append(file_vec)
+        properFiles.append(file)
 
     results = cosine_similarity(query_vector,file_vectors)[0]
 
-    return sorted(list(zip(files,results)),key = lambda x: x[1],reverse = True)
+    return sorted(list(zip(properFiles,results)),key = lambda x: x[1],reverse = True)
 
 
 def store_vector(uid,file):
 
     vectorizer = pickle.load(open("tfidf1.pkl", 'rb'))
 
-    user_pdf_ref.update({u'pid': firestore.ArrayUnion([file])})
+    user_pdf_ref.document(uid).update({u'pid': firestore.ArrayUnion([file])})
 
     file_details = pdfs_ref.document(file).get().to_dict()
     url = file_details['pdfUrl']
-    urllib.request.urlretrieve(url, "download.pdf")
-    infile = open('download.pdf', 'rb')
+
+    try:
+        urllib.request.urlretrieve(url, "download.pdf")
+        infile = open('download.pdf', 'rb')
+    except:
+        return False
 
     output = StringIO()
     manager = PDFResourceManager()
     converter = TextConverter(manager, output, laparams=LAParams())
     interpreter = PDFPageInterpreter(manager, converter)
-
+    
+    
     for page in PDFPage.get_pages(infile):
         interpreter.process_page(page)
     converter.close()
     text = output.getvalue()
     output.close
-
     
     file_vec = vectorizer.transform([text])
     file_vec_dic = dict(file_vec.todok().items())
-    file_vec_dic = {k[1],v for k,v in file_vec_dic}
+    file_vec_dic = {str(k[1]):str(v) for k,v in file_vec_dic.items()}
     
-    pdfs_ref.update({u'vector': file_vec_dic})
+    pdfs_ref.document(file).update({u'vector': file_vec_dic})
 
     return True
